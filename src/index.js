fb9e0f81f82876677cecf3e7017ed8019bd56cac
SainiCoder41 <sainicoder41@gmail.com>
2025-10-02 21:24:28 +0530

    Second  Commit

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://student-feed-back-frontend.vercel.app/',
     credentials: true 
 }))
 
